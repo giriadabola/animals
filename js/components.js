147e029b4b17b07bebc5c5c262189c3af962590b
giriadabola <giriadabola@gmail.com>
2026-07-03 16:00:38 +0100

    Add files via upload

--- js/components.js
+++ js/components.js
@@ -1,4 +1,4 @@
-import { auth, db } from "./firebase-config.js";
+import { auth, db } from "./firebase-config.js?v=2";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 
--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,4 +16,3 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 export { db, auth };
- 
