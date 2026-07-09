91c3d0c5683eb42b9a63dc45acf41aa66db810f9
giriadabola <giriadabola@gmail.com>
2026-07-09 18:54:47 +0100

    Add files via upload

--- form/sw-register.js
+++ form/sw-register.js
@@ -1,7 +1,10 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./sw-admin.js', { scope: './form.html' })
-            .then(reg => console.log('Service Worker de Administração registrado:', reg.scope))
-            .catch(err => console.log('Erro ao registrar o Service Worker de Administração:', err));
-    });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw-admin.js', { scope: './' })
+      .then((reg) => {
+        console.log('Service Worker de Administração registado:', reg.scope);
+        if (reg.waiting) reg.waiting.postMessage({ type: 'SKIP_WAITING' });
+      })
+      .catch((err) => console.log('Erro ao registar o Service Worker de Administração:', err));
+  });
 }
